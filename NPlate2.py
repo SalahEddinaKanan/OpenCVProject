95c4ed33400f9b034209b57ed04581eabbf7b70c
Salah Eddin Kanan <52652328+SalahEddinaKanan@users.noreply.github.com>
2020-07-19 14:54:09 +0300

    Create NPlate2.py

---
+++ NPlate2.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+framewidth = 640
+frameheight = 480
+nPlateCascade = cv2.CascadeClassifier("Resources/haarcascade_russian_plate_number.xml")
+minArea = 500
+color = (255,0,255)
+
+
+
+cap = cv2.VideoCapture(1)  #front camera use 0 instead of 1 
+cap.set(3, framewidth)
+cap.set(4, frameheight)
+cap.set(10, 150)
+count = 0
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 200, 200)
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgCanny, kernel, iterations=2)
+    imgThres = cv2.erode(imgDial, kernel, iterations=1)
+    return imgThres
+
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    numberPlates = nPlateCascade.detectMultiScale(imgGray, 1.1, 4)
+
+    for (x, y, w, h) in numberPlates:
+        area = w * h
+        if area > minArea:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2) #draw rectangle
+            cv2.putText(img, "Number Plate", (x, y - 5),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)   #put text under
+            imgRoi = img[y:y + h, x:x + w]
+            cv2.imshow("ROI", imgRoi)
+
+    cv2.imshow("Result", img)
+
+    if cv2.waitKey(1) and 0xFF == ord('s'):    #save function by pressing 'S'
+        cv2.imwrite("Resources/Scanned/NoPlate_"+str(count)+".jpg",imgRoi)
+        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
+        cv2.putText(img, "Scan saved", (150, 255), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 255), 2)
+
+        cv2.imshow("Results", img)
+        cv2.waitKey(500)
+        count+=1

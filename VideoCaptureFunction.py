9bf987631b23b7f3c47547b8fd5f36385bba22da
Salah Eddin Kanan <52652328+SalahEddinaKanan@users.noreply.github.com>
2020-07-19 12:49:04 +0300

    Add files via upload

---
+++ VideoCaptureFunction.py
@@ -0,0 +1,15 @@
+import cv2
+
+framewidth = 640
+frameheight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, framewidth)
+cap.set(4, frameheight)
+cap.set(10, 130)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+

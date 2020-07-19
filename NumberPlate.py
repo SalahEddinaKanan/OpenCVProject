c72488783521547b6fea3f5acecc3934c6394742
Salah Eddin Kanan <52652328+SalahEddinaKanan@users.noreply.github.com>
2020-07-19 14:48:21 +0300

    Update NumberPlate.py

--- NumberPlate.py
+++ NumberPlate.py
@@ -1,14 +1,14 @@
 import cv2
 
-######
+
 frameWidth = 640
 frameHeight = 480
 nPlateCascade = cv2.CascadeClassifier("Resources/haarcascade_chinese_plate_number.xml")
 minArea = 200
 color = (255, 0, 255)
-#######
 
-cap = cv2.VideoCapture("Resources/video12.mp4")
+
+cap = cv2.VideoCapture(1)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
